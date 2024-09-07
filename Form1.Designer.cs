fad3400fcb8c0c3e508f1f88696d2a151d618325
Helberte <helberteads@gmail.com>
2024-09-07 15:42:01 -0400

    build: adicionando pacote materialskin.2

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,10 +28,16 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            SuspendLayout();
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(932, 499);
+            Name = "Form1";
+            Text = "Controle de vendas e comissões";
+            ResumeLayout(false);
         }
 
         #endregion
--- Form1.cs
+++ Form1.cs
@@ -1,10 +1,18 @@
+using MaterialSkin;
+using MaterialSkin.Controls;
+
 namespace controle_vendas_comissoes
 {
-    public partial class Form1 : Form
+    public partial class Form1 : MaterialForm
     {
         public Form1()
         {
             InitializeComponent();
+
+            var materialSkinManager     = MaterialSkinManager.Instance;
+            materialSkinManager.AddFormToManage(this);
+            materialSkinManager.Theme   = MaterialSkinManager.Themes.LIGHT;
+            materialSkinManager.ColorScheme = new ColorScheme(Primary.BlueGrey800, Primary.BlueGrey900, Primary.BlueGrey500, Accent.LightBlue200, TextShade.WHITE);
         }
     }
 }
