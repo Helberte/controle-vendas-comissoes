a05d67fb5146bb57fc475c7f98ea19f7681f2db5
Helberte Costa Arruda <helberteads@gmail.com>
2024-10-03 17:42:22 -0400

    modularizando o projeto

---
+++ Model/Db/Entidades/Estado.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace controle_vendas_comissoes.Model.Db.Entidades
+{
+    [Table("estado")]
+    public class Estado : EntidadeBase
+    {
+        public int Id { get; set; }
+
+        public string Nome { get; set; } = string.Empty;
+
+        [Column("UF")]
+        public string UF { get; set; } = string.Empty;
+    }
+}
