33fa65cbd75a6b047d68ff2ea95b5bd1c823db48
Helberte <helberteads@gmail.com>
2024-11-13 21:14:07 -0400

    feat: models pedido de vendas

---
+++ Model/Db/Entidades/PessoaPedidoVenda.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace controle_vendas_comissoes.Model.Db.Entidades
+{
+    [Table("pessoa_pedido_venda")]
+    public class PessoaPedidoVenda : EntidadeBase
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Column("pedido_venda_id")]
+        public int PedidoVendaId { get; set; }
+
+        [Column("pessoa_id")]
+        public int PessoaId { get; set; }
+    }
+}
+ 
