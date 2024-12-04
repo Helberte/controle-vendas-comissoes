c46f9ed9d22832f024f08507dfd596134da68b23
helberte <helberteads@gmail.com>
2024-12-04 18:02:59 -0400

    feat: inicio adicao produto na venda

--- Model/Db/Entidades/PedidoVendaItem.cs
+++ Model/Db/Entidades/PedidoVendaItem.cs
@@ -15,14 +15,24 @@ namespace controle_vendas_comissoes.Model.Db.Entidades
         [Column("total", TypeName = "decimal(12, 4)")]
         public decimal Total { get; set; }
 
+        [Column("total_com_desconto", TypeName = "decimal(12, 4)")]
+        public decimal TotalComDesconto { get; set; }
+
         [Column("porcentagem_desconto", TypeName = "decimal(12, 4)")]
         public decimal PorcentagemDesconto { get; set; }
+
+        [Column("valor_desconto", TypeName = "decimal(12, 4)")]
+        public decimal ValorDesconto { get; set; }
 
         [Column("pedido_venda_id")]
         public int PedidoVendaId { get; set; }
          
         [Column("produto_id")]
         public int ProdutoId { get; set; }
+
+        public Produto? Produto { get; set; }
+
+        public PedidoVenda? PedidoVenda { get; set; }
     }
 }
  
